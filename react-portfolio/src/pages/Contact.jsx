b456de9b418d2f1775c553cd8f6dbdcb7e5b2e99
Fin M <qbres333@hotmail.com>
2024-11-27 04:27:48 -0500

    fix success message in handleSubmit, troubleshoot email validation

--- react-portfolio/src/pages/Contact.jsx
+++ react-portfolio/src/pages/Contact.jsx
@@ -9,8 +9,6 @@ export default function Contact() {
   const [validated, setValidated] = useState(false);
   // set initial state of email validation error
   const [emailError, setEmailError] = useState('');
-  // set initial state of sent email message
-  const [successMessage, setSuccessMessage] = useState("");
   // set initial state of contact form
   const [formData, setFormData] = useState({
     name: "",
@@ -18,25 +16,18 @@ export default function Contact() {
     message: "",
   });
 
-  // add async await for form submission so email request is sent to the email server upon form submission (not earlier)
+  // add async for form submission so email request is sent to the email server upon form submission (not earlier)
   const handleSubmit = async (event) => {
-    event.preventDefault();
+    // event.preventDefault();
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
       event.preventDefault();
       event.stopPropagation();
-      return;
+      // return;
     } else {
-
-      setValidated(true);
-      setSuccessMessage("Message sent successfully!");
-      // setFormData({
-      //   name: "",
-      //   email: "",
-      //   message: "",
-      // });
-
+      alert("Message sent successfully!");
     }
+    setValidated(true);
 
   };
 
@@ -52,53 +43,79 @@ export default function Contact() {
         onSubmit={handleSubmit}
         className="col-6 form contact-form mt-4"
       >
-        <Form.Group
-          className="form-group"
-          hasvalidation="true"
-        >
+        <Form.Group className="form-group" hasvalidation="true">
           <Form.Label htmlFor="contact-name">
             <b>Name:</b>
           </Form.Label>
           {/*dev.to docs; set field value as input using onChange function */}
-          <Form.Control required type="text" name="name" id="contact-name" value={formData.name} onChange={(event) => setFormData({...formData, name: event.target.value})} />
+          <Form.Control
+            required
+            type="text"
+            name="name"
+            id="contact-name"
+            value={formData.name}
+            onChange={(event) =>
+              setFormData({ ...formData, name: event.target.value })
+            }
+          />
           <Form.Control.Feedback type="invalid">
             Name is required
           </Form.Control.Feedback>
         </Form.Group>
 
-        <Form.Group
-          className="form-group mt-3"
-          hasvalidation="true"
-        >
-          <Form.Label htmlFor="contact-email" >
+        <Form.Group className="form-group mt-3" hasvalidation="true">
+          <Form.Label htmlFor="contact-email">
             <b>Email Address:</b>
           </Form.Label>
-          <Form.Control required type="email" name="email" id="contact-email" value={formData.email} onChange={(event) => {
-            const email = event.target.value;
-            const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g; //https://regexr.com/3e48o
-            setEmailError(emailRegex.test(email) ? '' : 'Email format is invalid')
-            setFormData({ ...formData, email: event.target.value });
-          }
-        } />
-          <Form.Control.Feedback type="invalid">
-            {emailError}
+          <Form.Control
+            required
+            type="email"
+            name="email"
+            id="contact-email"
+            value={formData.email}
+            onChange={(event) => {
+              const email = event.target.value;
+              const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g; //https://regexr.com/3e48o
+
+              // Check for invalid format
+              const errorMessage = emailRegex.test(email)
+                ? ""
+                : "Email is required";
+              setEmailError(errorMessage);
+              setFormData({ ...formData, email: event.target.value });
+            }}
+          />
+          <Form.Control.Feedback type="invalid" value={setEmailError}>
+            Email is required
           </Form.Control.Feedback>
         </Form.Group>
 
-        <Form.Group
-          className="form-group mt-3"
-          hasvalidation="true"
-        >
+        <Form.Group className="form-group mt-3" hasvalidation="true">
           <Form.Label htmlFor="contact-message">
             <b>Message:</b>
           </Form.Label>
-          <Form.Control required as="textarea" rows={5} name="message" id="contact-message" value={formData.message} onChange={(event) => setFormData({...formData, message: event.target.value})} />
+          <Form.Control
+            required
+            as="textarea"
+            rows={5}
+            name="message"
+            id="contact-message"
+            value={formData.message}
+            onChange={(event) =>
+              setFormData({ ...formData, message: event.target.value })
+            }
+          />
           <Form.Control.Feedback type="invalid">
             Message is required
           </Form.Control.Feedback>
         </Form.Group>
-        {successMessage && <p>{successMessage}</p>}
-        <Button type="submit" variant="secondary" className="col-12 mt-4" id="submit-email" >
+        {/* {successMessage} */}
+        <Button
+          type="submit"
+          variant="secondary"
+          className="col-12 mt-4"
+          id="submit-email"
+        >
           Submit
         </Button>
       </Form>
